cc22f3a8dae7a383c9b74b7d9b1264f33264c9f6
blackdaveblackdave <blackdave@me.com>
2024-10-22 18:32:03 -0400

    just updating

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const myEditionDropContractAddress: string = "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
+export const myEditionDropContractAddress: string = "0xa7A20AE933811f7e20DD226EFC96bC1E60AC4D10";
 
 // Replace this with your token id
 export const tokenId: string = "0";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,13 @@ import Head from "next/head";
 import ThirdwebGuideFooter from "../components/GitHubLink";
 
 // This is the chain your dApp will work on.
-const activeChain = "mumbai";
+const activeChain = "base";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
       <Head>
-        <title>thirdweb Edition Drop Minting Customizable Page</title>
+        <title>Black Dave Token</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
